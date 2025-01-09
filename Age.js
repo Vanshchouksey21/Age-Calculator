283f3989eecc58433ca409491051cbb2ac4b685c
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-01-09 16:07:18 +0530

    Age calsi

---
+++ Age.js
@@ -0,0 +1,60 @@
+const Display = () => {
+    let userageYear = document.querySelector("#AgeYear");
+    let userAgeMonth = document.querySelector("#AgeMonth");
+    let userAgeDay = document.querySelector("#AgeDay");
+    let Chnage=document.querySelector("#Chnage")
+    let userAge = document.querySelector("#AgeInput").value;
+
+    let errors = document.querySelector("#emptys");
+    if (userAge === "") {
+        errors.innerHTML = "Please Select Your Birth Date";
+        errors.style.color = "red";
+        userageYear.innerHTML = "";
+        userAgeMonth.innerHTML = "";
+        userAgeDay.innerHTML = "";
+        return;
+    } else {
+        errors.innerHTML = ""; 
+        errors.style.background = "transparent"; 
+        Chnage.innerHTML="Your age is " 
+    }
+
+    
+    let currentDates = new Date();
+    let currentDate = currentDates.getDate();
+    let currentMonth = currentDates.getMonth(); 
+    let currentYear = currentDates.getFullYear();
+
+
+    let userDates = new Date(userAge);
+    let userDate = userDates.getDate();
+    let userMonth = userDates.getMonth(); 
+    let userYear = userDates.getFullYear();
+
+  
+    let ageYear = currentYear - userYear;
+    let ageMonth = currentMonth - userMonth;
+    let ageDay = currentDate - userDate;
+
+    
+    if (ageDay < 0) {
+        ageMonth--;
+        ageDay += new Date(currentYear, currentMonth, 0).getDate();  
+    }
+
+    if (ageMonth < 0) {
+        ageYear--;
+        ageMonth += 12;  
+    }
+
+    // Displaying the Results:
+    userageYear.innerHTML = ageYear;
+    userAgeMonth.innerHTML = ageMonth;
+    userAgeDay.innerHTML = ageDay;
+}
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Vanshchouksey21/Age-Calculator.git
+// git push -u origin main
